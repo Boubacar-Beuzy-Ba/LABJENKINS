cd1944727ef4d560851c81e5657ffed792250d1c
Boubacar-Beuzy-Ba <bouba0178@gmail.com>
2024-01-13 21:53:42 +0000

    Basic counter app done with tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'labjenkins';
+  counter = 0;
+
+  increment() {
+    return this.counter++;
+  }
+
+  decrement() {
+    return this.counter--;
+  }
+
+  reset() {
+    return (this.counter = 0);
+  }
 }
