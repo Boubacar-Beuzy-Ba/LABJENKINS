cbfc749d99d9bded007c9c1d42602dcbf609eaa8
Boubacar-Beuzy-Ba <bouba0178@gmail.com>
2024-01-14 09:43:20 +0000

    removed 1 test in specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('labjenkins app is running');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'labjenkins app is running'
-    );
-  });
 });
