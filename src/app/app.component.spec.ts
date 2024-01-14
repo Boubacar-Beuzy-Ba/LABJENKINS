7ef2e4623049256f3efe5298e287342baccdd436
Boubacar-Beuzy-Ba <bouba0178@gmail.com>
2024-01-14 09:31:53 +0000

    title text updated in specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'labjenkins app is running!'`, () => {
+  it(`should have as title 'labjenkins app is running'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('labjenkins app is running!');
+    expect(app.title).toEqual('labjenkins app is running');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'labjenkins app is running!'
+      'labjenkins app is running'
     );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'labjenkins app is running!';
+  title = 'labjenkins app is running';
   counter = 0;
 
   increment() {
