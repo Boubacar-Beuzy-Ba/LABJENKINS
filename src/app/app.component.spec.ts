87b8e6a5b03df569f1fa24b1aee5d68a286fb93a
Boubacar-Beuzy-Ba <bouba0178@gmail.com>
2024-01-14 09:26:36 +0000

    specs updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,9 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'labjenkins'`, () => {
+  it(`should have as title 'labjenkins app is running!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('labjenkins');
+    expect(app.title).toEqual('labjenkins app is running!');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'labjenkins app is running!'
+    );
   });
 });
